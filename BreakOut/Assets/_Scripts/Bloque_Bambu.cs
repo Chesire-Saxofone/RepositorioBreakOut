b8b0817addce65740168d230150a94f39baa94b0
Chesire Saxofone <josefeospina2014@gmail.com>
2024-12-13 19:43:30 -0500

    Entrega Modulo 10
    Agrego la entrega de los nuevos bloques para el juego break out.

--- BreakOut/Assets/_Scripts/Bloque_Bambu.cs
+++ BreakOut/Assets/_Scripts/Bloque_Bambu.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloque_Piedra : Bloque
+public class Bloque_Bambu : Bloque
 {
     // Start is called before the first frame update
     void Start()
     {
-        resistencia = 5; 
+        //Es un Bloque Mas largo y con mas resistencia que la madera
+        resistencia = 4;
     }
 
     public override void RebotarBola()
     {
         base.RebotarBola();
     }
-
 }
--- BreakOut/Assets/_Scripts/Bloque_Goma.cs
+++ BreakOut/Assets/_Scripts/Bloque_Goma.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bloque_Piedra : Bloque
+public class Bloque_Goma : Bloque
 {
     // Start is called before the first frame update
     void Start()
     {
-        resistencia = 5; 
+        resistencia = 2; 
     }
 
     public override void RebotarBola()
     {
+        //la bola tendrá un mayor rebote. Será un bloque de mayor
+        //dificultad por lo que habrá pocos.
+        //aun por implementar cuando aprenda sobre el metodo de rebote.
         base.RebotarBola();
     }
 
