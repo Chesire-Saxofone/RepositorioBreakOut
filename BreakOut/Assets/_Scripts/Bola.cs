c927b7eae9db169f5bcc567564fac7c8d861e3bf
Chesire Saxofone <josefeospina2014@gmail.com>
2024-12-17 18:27:27 -0500

    Modulo 11 Entrega
    Agrega la entrega de los ejercicios de tipo input en unity.

--- BreakOut/Assets/_Scripts/Bola.cs
+++ BreakOut/Assets/_Scripts/Bola.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+    bool isTheGameStarted = false;
+    [SerializeField] public float velocidadBola = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space)|| Input.GetButton("Submit"))
+        {
+            if (!isTheGameStarted)
+            { 
+            isTheGameStarted=true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().velocity = velocidadBola * Vector3.up;
+            }
+        }
     }
 }
--- BreakOut/Assets/_Scripts/Jugador.cs
+++ BreakOut/Assets/_Scripts/Jugador.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+    [SerializeField] public int limitex = 23;
+    [SerializeField] public float velocidadPad = 1f;
+
+    Vector3 jugadorPos2d;
+    Vector3 jugadorPos3d;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //jugadorPos2d = Input.mousePosition;
+        //jugadorPos2d.z = -Camera.main.transform.position.z;
+        //jugadorPos3d = Camera.main.ScreenToWorldPoint(jugadorPos2d);
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    transform.Translate(Vector3.down * velocidadPad * Time.deltaTime);
+        //}
+        //else if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    transform.Translate(Vector3.up * velocidadPad * Time.deltaTime);
+        //}
+
+
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPad * Time.deltaTime);
+
+        Vector3 pos = this.transform.position;
+
+     
+        //pos.x = jugadorPos3d.x;
+        if (pos.x < -23)
+            pos.x = -limitex;
+        else if (pos.x > limitex)
+            pos.x = limitex;
+
+
+        this.transform.position = pos;
     }
 }
